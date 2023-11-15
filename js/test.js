659ca01ff45f4679e4189915350744078f203d75
YuriiRogovets <yura.rogovets@gmail.com>
2023-11-16 01:02:50 +0200

    did task-1

---
+++ js/test.js
@@ -0,0 +1,18 @@
+
+// function getAllPropValues(propName) {
+//   const products = [
+//     { name: "Radar", price: 1300, quantity: 4 },
+//     { name: "Scanner", price: 2700, quantity: 3 },
+//     { name: "Droid", price: 400, quantity: 7 },
+//     { name: "Grip", price: 1200, quantity: 9 },
+//     ];
+    
+//     const array = [];
+//    for (const product of products) {
+//        array.push(product[propName]);
+//     };
+//     return array;
+// };
+
+// console.log(getAllPropValues("nme"));
+    
